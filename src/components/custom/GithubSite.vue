02aa89d715b4d54b257a971ae989189e66e323b0
fungoindia <83899086+fungoindia@users.noreply.github.com>
2023-03-09 11:56:57 +0800

    Add files via upload

---
+++ src/components/custom/GithubSite.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="text-neutral-400">
+    <span>您还未添加API Key</span>
+    <!-- <a href="https://github.com/Chanzhaoyu/chatgpt-bot" target="_blank" class="text-blue-500">
+      GitHub
+    </a> -->
+  </div>
+</template>
